25cd308acd2af7cf2aaba2b9adc710dbd182cb66
VictorEne-codes <omalevicene@gmail.com>
2023-09-26 08:48:25 -1200

    created nav bar

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="homestyling.css">
+</head>
+<body>
+<?php 
+    require_once "nav.php" 
+    ?>
+    <div class="right-side">
+        <h1>PEDIFORTE SHOPPING COMPLEX</h1>
+    </div>
+    
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="navstyling.css">
+</head>
+<body>
+    <div class="container">
+        <div class="left-side">
+        <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="categories.php">Categories</a></li>
+            <li><a href="products.php">Products</a></li>
+            <li><a href="sales.php">Sales</a></li>
+            <li><a href="salesreport.php">Sales Report</a></li>
+        </ul>
+    </nav>
+        </div>
+    </div>
+</body>
+</html>
